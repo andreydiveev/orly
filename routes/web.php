f9063b03dd7fa0dbf18a0812fe17d9e3c89e97e5
Andrey Diveev <andrey.diveev@gmail.com>
2017-12-09 19:11:48 +0300

    Require bot lib

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->get('/entrypointi6l9bpCsguYpc', function () use ($router) {
+    return $router->app->version();
+});
+
