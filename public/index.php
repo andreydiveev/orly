68452b3e5ae09026514b8fcb7598c9edf167334f
Andrey Diveev <andrey.diveev@gmail.com>
2017-12-10 17:33:40 +0300

    test

--- public/index.php
+++ public/index.php
@@ -1,17 +1,11 @@
 <?php
 require_once "../vendor/autoload.php";
 
-echo 123;
-
 try {
     $bot = new \TelegramBot\Api\Client(getenv('TELEGRAM_BOT_TOKEN'));
-    $bot->command('devanswer', function ($message) use ($bot) {
-        preg_match_all('/{"text":"(.*?)",/s', file_get_contents('http://devanswers.ru/'), $result);
-        $bot->sendMessage($message->getChat()->getId(),
-            str_replace("<br/>", "\n", json_decode('"' . $result[1][0] . '"')));
-    });
-    $bot->command('qaanswer', function ($message) use ($bot) {
-        $bot->sendMessage($message->getChat()->getId(), file_get_contents('http://qaanswers.ru/qwe.php'));
+
+    $bot->command('ping', function ($message) use ($bot) {
+        $bot->sendMessage($message->getChat()->getId(), 'pong');
     });
     $bot->run();
 } catch (\TelegramBot\Api\Exception $e) {
