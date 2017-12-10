2ccee3470dcd71b2b199320a53b3d77d10a634b2
andreydiveev <flat-sea@ya.ru>
2017-12-10 17:46:24 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,9 @@ $router->post('/entrypointi6l9bpCsguYpc', function () use ($router) {
         // or initialize with botan.io tracker api key
         // $bot = new \TelegramBot\Api\Client('YOUR_BOT_API_TOKEN', 'YOUR_BOTAN_TRACKER_API_KEY');
 
-        $bot->command('ping', function ($message) use ($bot) {
-            $bot->sendMessage($message->getChat()->getId(), 'pong!');
+        $bot->command('start', function ($message) use ($bot) {
+            $msg = "Welcome!";
+            $bot->sendMessage($message->getChat()->getId(), $msg);
         });
 
         $bot->run();
