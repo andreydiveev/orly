fb6a5a25665062244bbc17a27064403814166135
Andrey Diveev <andrey.diveev@gmail.com>
2017-12-10 17:40:24 +0300

    test

--- public/index.php
+++ public/index.php
@@ -1,13 +1,28 @@
 <?php
-require_once "../vendor/autoload.php";
 
-try {
-    $bot = new \TelegramBot\Api\Client(getenv('TELEGRAM_BOT_TOKEN'));
+/*
+|--------------------------------------------------------------------------
+| Create The Application
+|--------------------------------------------------------------------------
+|
+| First we need to get an application instance. This creates an instance
+| of the application / container and bootstraps the application so it
+| is ready to receive HTTP / Console requests from the environment.
+|
+*/
 
-    $bot->command('ping', function ($message) use ($bot) {
-        $bot->sendMessage($message->getChat()->getId(), 'pong');
-    });
-    $bot->run();
-} catch (\TelegramBot\Api\Exception $e) {
-    $e->getMessage();
-}
+$app = require __DIR__.'/../bootstrap/app.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request
+| through the kernel, and send the associated response back to
+| the client's browser allowing them to enjoy the creative
+| and wonderful application we have prepared for them.
+|
+*/
+
+$app->run();
